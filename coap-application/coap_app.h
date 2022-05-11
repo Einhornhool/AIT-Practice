df2ab626599e55052ebfe0278b0c11304acbe92b
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2022-05-11 11:49:28 +0200

    Add code basis

---
+++ coap-application/coap_app.h
@@ -0,0 +1,24 @@
+#ifndef COAP_APP_H
+#define COAP_APP_H
+
+#include "fmt.h"
+#include "net/gcoap.h"
+#include "net/utils.h"
+
+
+/**
+ * @brief   Shell interface exposing the client side features of gcoap
+ * @param   argc    Number of shell arguments (including shell command name)
+ * @param   argv    Shell argument values (including shell command name)
+ * @return  Exit status of the shell command
+ */
+int coap_saul_cli_cmd(int argc, char **argv);
+
+/**
+ * @brief   Registers the CoAP resources exposed in the example app
+ *
+ * Run this exactly one during startup.
+ */
+void server_init(void);
+
+#endif
--- coap-application/main.c
+++ coap-application/main.c
@@ -1,15 +1,25 @@
 #include "net/gcoap.h"
 #include "shell.h"
+#include "coap_app.h"
 
 #define ENABLE_DEBUG 0
 #include "debug.h"
+
+#define MAIN_QUEUE_SIZE (4)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+static const shell_command_t shell_commands[] = {
+    { "saul-coap", "Control Sensors and LEDs via CoAP and SAUL", coap_saul_cli_cmd },
+    { NULL, NULL, NULL }
+};
+
 
 int main(void)
 {
     puts("Start AIT CoAP Application");
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
