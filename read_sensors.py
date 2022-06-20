1d137ed5c6676ba9e790e036e7f5afd8503b66df
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2022-06-20 15:28:53 +0200

    Update script

--- read_sensors.py
+++ read_sensors.py
@@ -1,30 +1,50 @@
 from aiocoap import *
 import asyncio
+import re
+
+async def send_request(request, protocol):
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+
+    if response.code.is_successful():
+        return response.payload
+
+    return None
+
+def get_address(payload):
+    payload = payload.split(";")
+    for s in payload:
+        if re.match("^base", s):
+            return s.split("=")[1]
 
 async def request_resources():
     protocol = await Context.create_client_context()
 
     request = Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:4000:0:1]/.well-known/core')
+    payload = send_request(request, protocol)
 
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
-
-    if not response.code.is_successful():
-        print(f'Request failed: {response.code}')
+    if payload == None:
+        print(f'/.well-ḱnown/core request failed')
         return
 
     request = Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:4000:0:1]/endpoint-lookup/')
+    payload = send_request(request, payload)
+    if payload == None:
+        print(f'/endpoint-lookup/ request failed')
+        return
 
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
+    addr = get_address(payload)
 
-    print(f'Result: {response.payload}')
+    request = Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:4000:0:1]/endpoint-lookup/')
+    payload = send_request(request, payload)
+    if payload == None:
+        print(f'Resource request failed')
+        return
+
+    print(f'Result: {payload}')
 
 if __name__ == '__main__':
     asyncio.run(request_resources())
