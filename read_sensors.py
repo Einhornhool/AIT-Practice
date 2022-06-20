390ca56d1c9257466df2c098166bdda9a2edc88c
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2022-06-20 14:55:20 +0200

    Update script

--- read_sensors.py
+++ read_sensors.py
@@ -12,9 +12,19 @@ async def request_resources():
         print('Failed to fetch resource:')
         print(e)
 
-    if response.code.is_successful():
-        payload = str(response.payload)
-        print(f'Result: {payload.split(",")}')
+    if not response.code.is_successful():
+        print(f'Request failed: {response.code}')
+        return
+
+    request = Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:4000:0:1]/endpoint-lookup/')
+
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+
+    print(f'Result: {response.payload}')
 
 if __name__ == '__main__':
     asyncio.run(request_resources())
