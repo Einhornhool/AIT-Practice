140a9637853cc305179c800e9fa453cc41c9f5b8
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2022-06-20 14:33:00 +0200

    Add python script

---
+++ read_sensors.py
@@ -0,0 +1,18 @@
+from aiocoap import *
+import asyncio
+
+async def request_resources():
+    protocol = await Context.create_client_context()
+
+    request = Message(code=GET, uri='coap://[2001:67c:254:b0b2:affe:4000:0:1]/.well-known/core')
+
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print(f'Result: {response.code}: {response.payload}')
+
+if __name__ == '__main__':
+    asyncio.run(request_resources())
